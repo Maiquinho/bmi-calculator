c86ba6b7b2f2cbc6dc15526891fc8d2f968905f4
Michael Matheus <michaelmatheus2001@gmail.com>
2022-12-29 14:04:53 -0300

    feat: form calculator base

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import styles from './App.module.css';
+import { Form } from './components/Form';
 import { Header } from './components/Header';
 
 export default function App() {
   return (
     <div className={styles.main}>
       <Header />
+
       <div className={styles.container}>
+
         <div className={styles.column}>
-        Col 1
+
+          <h1>Calcule o seu IMC.</h1>
+          <p>IMC é a sigla para Indice de Massa Corpórea, parâmetro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa.</p>
+
+         <Form />
         </div>
+
         <div className={styles.column}>
         Col 2
         </div>
+
       </div>
     </div>
   )
---
+++ src/components/Form/index.tsx
@@ -0,0 +1,36 @@
+import { FormEvent, useState } from 'react';
+import styles from './styles.module.css';
+
+export function Form(){
+    const [heightField, setHeightField] = useState<number>(0);
+    const [weightField, setWeightField] = useState<number>(0);
+
+
+    function handleCalculateForm(e: FormEvent){
+        e.preventDefault();
+
+        if(heightField && weightField){
+            alert('é sobre isse')
+        }
+    }
+
+
+    return(
+        <form className={styles.form} onSubmit={handleCalculateForm}>
+            <input 
+                type="number" 
+                placeholder="Digite a sua altura. Ex: 1.5 (em metros)"
+                value={heightField > 0 ? heightField : ''}
+                onChange={e => setHeightField(parseFloat(e.target.value))}
+            />
+            <input 
+                type="number" 
+                placeholder="Digite a seu peso. Ex: 75.3 (em Kg)"
+                value={weightField > 0 ? weightField : ''}
+                onChange={e => setWeightField(parseFloat(e.target.value))}
+            />
+
+            <button type="submit">Calcular</button>
+        </form>
+    )
+}
