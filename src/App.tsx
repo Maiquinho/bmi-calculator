f5fd5c06fbf0346e158659d57f547382d2288d47
Michael Matheus <michaelmatheus2001@gmail.com>
2022-12-29 17:49:00 -0300

    feat: card styles

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ export default function App() {
       <div className={styles.container}>
         <div className={styles.column}>
           <h1>Calcule o seu IMC.</h1>
-          <p>IMC é a sigla para Indice de Massa Corpórea, parâmetro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa.</p>
+          <p>IMC é a sigla para Índice de Massa Corporal, parâmetro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa.</p>
           
          <Form />
         </div>
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,7 @@
 import { bmiLevel } from '../../helpers/bmi';
 import styles from './styles.module.css';
+import upImage from '../../assets/up.png';
+import downImage from '../../assets/down.png';
 
 
 interface CardProps {
@@ -9,6 +11,16 @@ interface CardProps {
 
 export function Card({ item }: CardProps){
     return(
-        <div>Card</div>
+        <div className={styles.card} style={{ backgroundColor: item.color }}>
+            <div className={styles.cardIcon}>
+                <img src={item.icon === 'up' ? upImage : downImage } width={30} alt="BMI icon" />
+            </div>
+            <div className={styles.cardTitle}>
+                {item.title}
+            </div>
+            <div className={styles.cardInfo}>
+                IMC está entre <strong>{item.bmi[0]}</strong> e <strong>{item.bmi[1]}</strong>
+            </div>
+        </div>
     )
 }
