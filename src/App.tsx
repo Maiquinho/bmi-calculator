26a029f2757edd2be647fda276555d5a8f594969
Michael Matheus <michaelmatheus2001@gmail.com>
2022-12-29 08:23:27 -0300

    feat: layout base grid system

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import styles from './App.module.css';
+import { Header } from './components/Header';
+
 export default function App() {
   return (
-    <div>
-      BMI Calculator
+    <div className={styles.main}>
+      <Header />
+      <div className={styles.container}>
+        <div className={styles.column}>
+        Col 1
+        </div>
+        <div className={styles.column}>
+        Col 2
+        </div>
+      </div>
     </div>
   )
 }
